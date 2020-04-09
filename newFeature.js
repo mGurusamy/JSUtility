427cfb6148f14b0cbcfca2e1e4b6d9fb701d0615
Manoharan Gurusamy <manoharan.gurusamy@gmail.com>
2020-04-09 23:11:33 +0100

    This is going to be combined commit message for all squashed commits
    change2
    
    change3

--- newFeature.js
+++ newFeature.js
@@ -1,1 +1,4 @@
 //assume that hot fix applied over here!
+// change1 - Assume that this is going to be in change1
+// change2 - Assume that this is going to be in change2
+// change3 - Assume that this is going to be in change3
